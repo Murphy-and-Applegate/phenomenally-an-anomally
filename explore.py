5a364d33f08075c837e55a866c85974f4fb3ba52
Tyler-Applegate <tyler.applegate@gmail.com>
2021-07-21 13:09:00 -0500

    updated functions

--- explore.py
+++ explore.py
@@ -10,19 +10,105 @@ from IPython.display import display, display_html
 
 
 
-############### General Exploration Functions #############################
+############### Question 2 Functions #############################
+
+def remove_home(df):
+    '''
+    This function take in my prepped cohorts and curriculum logs DataFrame and removes all 'endpoint' that are '/', which is the homepage, and not an actual lesson.
+    '''
+    df = df[df['endpoint'] != '/']
+    
+    return df
+
+def q2_cols(df):
+    '''
+    This function takes in the result of my remove_home function, and returns a pandas DataFrame with 'endpoint', 'cohort', and 'program_id' as columns. All other variables are dropped since they are not needed for question 2.
+    '''
+    
+    df = df[['endpoint', 'cohort', 'program_id']]
+    
+    return df
 
 def split_by_program(df):
     '''
     This functions takes in my prepped cohorts and curriculum logs DataFrame and returns a separate DataFrame for each program.
     '''
     
-    df_1 = df[df['program_id'] ==1]
-    df_2 = df[df['program_id'] ==2]
-    df_3 = df[df['program_id'] ==3]
-    df_4 = df[df['program_id'] ==4]
+    df_1 = df[df['program_id'] ==1].drop(columns = 'program_id')
+    df_2 = df[df['program_id'] ==2].drop(columns = 'program_id')
+    df_3 = df[df['program_id'] ==3].drop(columns = 'program_id')
+    df_4 = df[df['program_id'] ==4].drop(columns = 'program_id')
     
     return df_1, df_2, df_3, df_4
+
+def endpoint_group(df_1, df_2, df_3, df_4):
+    '''
+    This function takes in my 4 pandas DataFrames split by program_id, and returns 
+    '''
+    
+    # Groupby 'endpoint' and get value counts for 'cohort'
+    # Create a 'count' column
+    df_1 = pd.DataFrame(df_1.groupby('endpoint')['cohort'].value_counts()).rename(columns = {'cohort':'count'})
+    df_2 = pd.DataFrame(df_2.groupby('endpoint')['cohort'].value_counts()).rename(columns = {'cohort':'count'})
+    df_3 = pd.DataFrame(df_3.groupby('endpoint')['cohort'].value_counts()).rename(columns = {'cohort':'count'})
+    df_4 = pd.DataFrame(df_4.groupby('endpoint')['cohort'].value_counts()).rename(columns = {'cohort':'count'})
+    
+    # Unstack the multi-level index, to return useable DataFrames
+    df_1 = df_1.reset_index(level=['endpoint', 'cohort'])
+    df_2 = df_2.reset_index(level=['endpoint', 'cohort'])
+    df_3 = df_3.reset_index(level=['endpoint', 'cohort'])
+    df_4 = df_4.reset_index(level=['endpoint', 'cohort'])
+    
+    return df_1, df_2, df_3, df_4
+
+def q2_prep(df):
+    '''
+    This functions combines all of my question 2 functions, and returns 4 pandas DataFrames ready to be explored in order to answer question 2.
+    '''
+    
+    # remove the '/' 'endpoint'
+    df = remove_home(df)
+    
+    # drop down to only necessary columns for question 2
+    df = q2_cols(df)
+    
+    # split into 4 pandas DataFrames, 1 for each program_id
+    df_1, df_2, df_3, df_4 = split_by_program(df)
+    
+    # give valueable tables for exploration grouped by 'endpoint'
+    df_1, df_2, df_3, df_4 = endpoint_group(df_1, df_2, df_3, df_4)
+    
+    return df_1, df_2, df_3, df_4
+
+def add_mm_range(df):
+    '''
+    This functions takes in a pandas DataFrame after it has gone through q2_prep. It adds 'min', 'max', and 'range' columns and returns a pandas DataFrame that allows easy viewing of results for question 2, broken down by program.
+    '''
+    # adds 'min' and 'max' cols for 'count' and 'cohort'
+    df_a = pd.DataFrame(df.groupby(['endpoint'])['count'].agg(['min']))
+    df_b = pd.DataFrame(df.groupby(['endpoint'])['cohort'].agg(['min']))
+    df_c = pd.DataFrame(df.groupby(['endpoint'])['count'].agg(['max']))
+    df_d = pd.DataFrame(df.groupby(['endpoint'])['cohort'].agg(['max']))
+    
+    # merges into 1 DataFrame
+    df_ab = pd.merge(df_a, df_b, how='left', on='endpoint')
+    df_cd = pd.merge(df_c, df_d, how='left', on='endpoint')
+    df = pd.merge(df_ab, df_cd, how='left', on='endpoint')
+    
+    # adds a 'range' column
+    df['range'] = df['max_x']-df['min_x']
+    df = df[df['min_x']>10].sort_values(by='range', ascending=False)
+    
+    return df
+
+############################ Question 6 Functions ########################################
+
+# Use remove_home function from q2
+# Use split_by_program function from q2
+
+    
+    
+
 
 
 
